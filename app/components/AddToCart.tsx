6880c8eab20d55299918f260e08d97708d9ce97c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-04 23:48:51 +0530

    setup done

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import React from "react";
 
-const AddToCart = () => {
-  return <div>
 
-    Hello
-  </div>;
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("added")}>Add To cart</button>
+      Add To cart
+    </div>
+  );
 };
 
 export default AddToCart;
--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,10 @@
-"use client";
 import React from "react";
+import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
     <div>
-      <button onClick={() => console.log("added")}>Add To cart</button>
+      <AddToCart />
     </div>
   );
 };
